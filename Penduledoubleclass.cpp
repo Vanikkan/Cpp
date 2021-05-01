893e5323a931e4d5015762ec17958efc2d46a4a3
Vanikkan <83508375+Vanikkan@users.noreply.github.com>
2021-05-01 22:42:04 +0200

    Update Penduledoubleclass.cpp

--- Penduledoubleclass.cpp
+++ Penduledoubleclass.cpp
@@ -9,13 +9,13 @@ using namespace std;
 
 class eqd {
 public:
-  float theta, dtheta, alpha, dalpha, M1, M2,  t, h, T, g, l1, l2, ktheta1, kalpha1, kdtheta1, kdalpha1, ktheta2, kalpha2, kdtheta2, kdalpha2, ktheta3, kalpha3, kdtheta3, kdalpha3, ktheta4, kalpha4, kdtheta4, kdalpha4 ;
+  double theta, dtheta, alpha, dalpha, M1, M2,  t, h, T, g, l1, l2, ktheta1, kalpha1, kdtheta1, kdalpha1, ktheta2, kalpha2, kdtheta2, kdalpha2, ktheta3, kalpha3, kdtheta3, kdalpha3, ktheta4, kalpha4, kdtheta4, kdalpha4 ;
 
-  float F(float t, float theta, float dtheta, float alpha, float dalpha) {
-    return (pow(dtheta,2)*M2*l1*cos((alpha-theta)*M_PI/180)*sin((alpha-theta)*M_PI/180)+pow(dalpha,2)*M2*l2*sin((alpha-theta)*M_PI/180)-(M1+M2)*g*sin(theta*M_PI/180)+M2*cos((alpha-theta)*M_PI/180)*g*sin(alpha*M_PI/180))/((M1+M2)*l1-M2*l2*pow(cos((alpha-theta)*M_PI/180),2));
+  double F(double t, double theta, double dtheta, double alpha, double dalpha) {
+    return (pow(dtheta,2)*M2*l1*cos((alpha-theta)*M_PI/180)*sin((alpha-theta)*M_PI/180)+pow(dalpha,2)*M2*l2*sin((alpha-theta)*M_PI/180)-(M1+M2)*g*sin(theta*M_PI/180)+M2*cos((alpha-theta)*M_PI/180)*g*sin(alpha*M_PI/180))/((M1+M2)*l1-M2*l1*pow(cos((alpha-theta)*M_PI/180),2));
   }
   
-  float G(float t, float theta, float dtheta, float alpha, float dalpha) {
+  double G(double t, double theta, double dtheta, double alpha, double dalpha) {
     return (-pow(dalpha,2)*M2*l2*cos((alpha-theta)*M_PI/180)*sin((alpha-theta)*M_PI/180)+(M1+M2)*(g*sin(theta*M_PI/180)*cos((alpha-theta)*M_PI/180)-l1*pow(dtheta,2)*sin((alpha-theta)*M_PI/180)-g*sin(alpha*M_PI/180)))/((M1+M2)*l2-M2*l2*pow(cos((alpha-theta)*M_PI/180),2));
   }
 };
@@ -24,7 +24,7 @@ public:
 class point {
   
  public:
-  float x, y;
+  double x, y;
 
 };
 
@@ -38,21 +38,21 @@ int main () {
   eqd eqd1;
   
   //On déclare nos variables:
-  
+
   //PAS
-  eqd1.h=0.1;
+  eqd1.h=0.001;
   
   eqd1.g=9.81;
   //Longeurs des deux fils
   eqd1.l1=1;
-  eqd1.l2=1;
+  eqd1.l2=2;
   
   //Masse des deux masses 
-  eqd1.M1=1;
-  eqd1.M2=1;
+  eqd1.M1=25;
+  eqd1.M2=25;
   
   //Conditions initiales pour le premier pendule
-  eqd1.theta=45;
+  eqd1.theta=90;
   eqd1.alpha=45;
   
   //Conditions initiale pour le deuxième pendule
@@ -99,33 +99,32 @@ int main () {
   
   while (eqd1.t<=eqd1.T)
     {
-  
+
   eqd1.ktheta1=eqd1.dtheta*eqd1.h;
   eqd1.kalpha1=eqd1.dalpha*eqd1.h;
   eqd1.kdtheta1=eqd1.F(eqd1.t, eqd1.theta, eqd1.alpha, eqd1.dtheta, eqd1.dalpha)*eqd1.h;
   eqd1.kdalpha1=eqd1.G(eqd1.t, eqd1.theta, eqd1.alpha, eqd1.dtheta, eqd1.dalpha)*eqd1.h;
 
-  eqd1.ktheta2=(eqd1.dtheta+(1/2)*eqd1.kdtheta1)*eqd1.h;
-  eqd1.kalpha2=(eqd1.dalpha+(1/2)*eqd1.kdalpha1)*eqd1.h;
-  eqd1.kdtheta2=eqd1.F(eqd1.t+ eqd1.h/2, eqd1.theta+eqd1.ktheta1/2, eqd1.alpha+eqd1.kalpha1/2, eqd1.dtheta+eqd1.kdtheta1/2, eqd1.dalpha+eqd1.kdalpha1/2)*eqd1.h;
-  eqd1.kdtheta2=eqd1.G(eqd1.t+ eqd1.h/2, eqd1.theta+eqd1.ktheta1/2, eqd1.alpha+eqd1.kalpha1/2, eqd1.dtheta+eqd1.kdtheta1/2, eqd1.dalpha+eqd1.kdalpha1/2)*eqd1.h;
+  eqd1.ktheta2=(eqd1.dtheta+eqd1.kdtheta1/2.)*eqd1.h;
+  eqd1.kalpha2=(eqd1.dalpha+eqd1.kdalpha1/2.)*eqd1.h;
+  eqd1.kdtheta2=eqd1.F(eqd1.t+ eqd1.h/2., eqd1.theta+eqd1.ktheta1/2., eqd1.alpha+eqd1.kalpha1/2., eqd1.dtheta+eqd1.kdtheta1/2., eqd1.dalpha+eqd1.kdalpha1/2.)*eqd1.h;
+  eqd1.kdtheta2=eqd1.G(eqd1.t+ eqd1.h/2., eqd1.theta+eqd1.ktheta1/2., eqd1.alpha+eqd1.kalpha1/2., eqd1.dtheta+eqd1.kdtheta1/2., eqd1.dalpha+eqd1.kdalpha1/2.)*eqd1.h;
 
-  eqd1.ktheta3=(eqd1.dtheta+(1/2)*eqd1.kdtheta2)*eqd1.h;
-  eqd1.kalpha3=(eqd1.dalpha+(1/2)*eqd1.kdalpha2)*eqd1.h;
-  eqd1.kdtheta3=eqd1.F(eqd1.t+ eqd1.h/2, eqd1.theta+eqd1.ktheta2/2, eqd1.alpha+eqd1.kalpha2/2, eqd1.dtheta+eqd1.kdtheta2/2, eqd1.dalpha+eqd1.kdalpha2/2)*eqd1.h;
-  eqd1.kdtheta3=eqd1.G(eqd1.t+ eqd1.h/2, eqd1.theta+eqd1.ktheta2/2, eqd1.alpha+eqd1.kalpha2/2, eqd1.dtheta+eqd1.kdtheta2/2, eqd1.dalpha+eqd1.kdalpha2/2)*eqd1.h;
+  eqd1.ktheta3=(eqd1.dtheta+eqd1.kdtheta2/2.)*eqd1.h;
+  eqd1.kalpha3=(eqd1.dalpha+eqd1.kdalpha2/2.)*eqd1.h;
+  eqd1.kdtheta3=eqd1.F(eqd1.t+ eqd1.h/2., eqd1.theta+eqd1.ktheta2/2., eqd1.alpha+eqd1.kalpha2/2., eqd1.dtheta+eqd1.kdtheta2/2., eqd1.dalpha+eqd1.kdalpha2/2.)*eqd1.h;
+  eqd1.kdtheta3=eqd1.G(eqd1.t+ eqd1.h/2., eqd1.theta+eqd1.ktheta2/2., eqd1.alpha+eqd1.kalpha2/2., eqd1.dtheta+eqd1.kdtheta2/2., eqd1.dalpha+eqd1.kdalpha2/2.)*eqd1.h;
 
   eqd1.ktheta4=(eqd1.dtheta+eqd1.kdtheta3)*eqd1.h;
   eqd1.kalpha4=(eqd1.dalpha+eqd1.kdalpha3)*eqd1.h;
   eqd1.kdtheta4=eqd1.F(eqd1.t+ eqd1.h, eqd1.theta+eqd1.ktheta3, eqd1.alpha+eqd1.kalpha3, eqd1.dtheta+eqd1.kdtheta3, eqd1.dalpha+eqd1.kdalpha3)*eqd1.h;
   eqd1.kdtheta4=eqd1.G(eqd1.t+ eqd1.h, eqd1.theta+eqd1.ktheta3, eqd1.alpha+eqd1.kalpha3, eqd1.dtheta+eqd1.kdtheta3, eqd1.dalpha+eqd1.kdalpha3)*eqd1.h;
 
-  eqd1.theta=eqd1.theta+(1/6)*(eqd1.ktheta1+2*eqd1.ktheta2+2*eqd1.ktheta3+eqd1.ktheta4);
-  eqd1.alpha=eqd1.alpha+(1/6)*(eqd1.kalpha1+2*eqd1.kalpha2+2*eqd1.kalpha3+eqd1.kalpha4);
-  eqd1.dtheta=eqd1.dtheta+(1/6)*(eqd1.kdtheta1+2*eqd1.kdtheta2+2*eqd1.kdtheta3+eqd1.kdtheta4);
-  eqd1.dalpha=eqd1.dalpha+(1/6)*(eqd1.kdalpha1+2*eqd1.kdalpha2+2*eqd1.kdalpha3+eqd1.kdalpha4);
-
-   
+  eqd1.theta=eqd1.theta+(eqd1.ktheta1+2*eqd1.ktheta2+2*eqd1.ktheta3+eqd1.ktheta4)/6.;
+  eqd1.alpha=eqd1.alpha+(eqd1.kalpha1+2*eqd1.kalpha2+2*eqd1.kalpha3+eqd1.kalpha4)/6.;
+  eqd1.dtheta=eqd1.dtheta+(eqd1.kdtheta1+2*eqd1.kdtheta2+2*eqd1.kdtheta3+eqd1.kdtheta4)/6.;
+  eqd1.dalpha=eqd1.dalpha+(eqd1.kdalpha1+2*eqd1.kdalpha2+2*eqd1.kdalpha3+eqd1.kdalpha4)/6.;
+  
   B.x=eqd1.l1*sin(eqd1.theta*M_PI/180);
   B.y=-eqd1.l1*cos(eqd1.theta*M_PI/180);
 
@@ -139,10 +138,30 @@ int main () {
   fichier1<<" "<<C.x<<" "<<C.y<<endl;
   fichier1<<" "<<endl;
   fichier1<<" "<<endl;
+  
   cout<<" "<<eqd1.t<<endl;
+
   eqd1.t=eqd1.t+eqd1.h;
   
  }
   
+    ofstream animation("animationpendule.gnu"); // création du fichier de sortie pour le gif
+
+    animation<<"set title \"Animation du pendule au cours du temps\""<<endl;
+    animation<<"set terminal gif animate delay "<<eqd1.h*100000<<endl;
+    animation<<"set output \"penduleanime.gif\"" <<endl;
+    animation<<"set xrange ["<<-(eqd1.l1+eqd1.l2)<<":"<<(eqd1.l1+eqd1.l2)<<"]"<<endl; //Permet d'adapter la fenetre à la longeur des fils en x
+   
+    animation<<"set yrange ["<<-(eqd1.l1+eqd1.l2)<<":"<<(eqd1.l1+eqd1.l2)<<"]"<<endl; //Permet d'adapter la fenetre à la longeur des fils en y
+   
+ 
+   
+    animation<<"set tics nomirror"<<endl; //Impossible d'avoir une image qui apparait 2 fois
+    animation<<"set pointintervalbox 3"<<endl; //Définis un style de ligne
+    animation<<"do for [i=1:999] {plot \"pendule_double_point.out\" index i using 1:2  title 'double-pendule' w linespoints ls 1}"<<endl;
+
+    system("gnuplot animationpendule.gnu");
+
+    
  return 0;
 }
